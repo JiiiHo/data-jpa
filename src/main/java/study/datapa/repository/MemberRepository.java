cdb0507938e164b87abf0ab1faf0b9c33688176e
SanhoOceanTech <tlswlgh120@naver.com>
2020-10-26 09:35:49 +0900

    pageing in Spring data jpa

--- src/main/java/study/datapa/repository/MemberRepository.java
+++ src/main/java/study/datapa/repository/MemberRepository.java
@@ -1,7 +1,18 @@
 package study.datapa.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import study.datapa.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    //
+    @Query(value = "select m" +
+            " from Member m" +
+            " left join m.team t",
+            countQuery = "select count(m.username)" +
+                    " from Member m") //countQuery를 분리할 수 있다. countQuery에는 조인이 필요 없으므로 따로 쓰면 성능 향상
+    Page<Member> findByAge(int age, Pageable pageable);
 }
--- src/test/java/study/datapa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datapa/repository/MemberRepositoryTest.java
@@ -3,6 +3,8 @@ package study.datapa.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datapa.entity.Member;
@@ -29,5 +31,39 @@ class MemberRepositoryTest {
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
         assertThat(findMember).isEqualTo(member);
+    }
+
+    @Test
+    public void testMember2() throws Exception {
+        Member member = Member.builder()
+                .username("test")
+                .age(10)
+                .build();
+        memberRepository.save(member);
+        member = Member.builder()
+                .username("test2")
+                .age(10)
+                .build();
+        memberRepository.save(member);
+        member = Member.builder()
+                .username("test3")
+                .age(10)
+                .build();
+        memberRepository.save(member);
+        member = Member.builder()
+                .username("test4")
+                .age(13)
+                .build();
+        memberRepository.save(member);
+        member = Member.builder()
+                .username("test5")
+                .age(10)
+                .build();
+        memberRepository.save(member);
+        PageRequest pageRequest = PageRequest.of(0, 3);
+        Page<Member> byAge = memberRepository.findByAge(10, pageRequest);
+        for (Member member1 : byAge) {
+            System.out.println("member : " + member1);
+        }
     }
 }
