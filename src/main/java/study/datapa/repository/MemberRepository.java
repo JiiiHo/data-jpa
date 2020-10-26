002cfdd676f54f77689eec19fab0189790e14771
SanhoOceanTech <tlswlgh120@naver.com>
2020-10-26 10:25:02 +0900

    EntityGraph fetch join without Query

--- src/main/java/study/datapa/repository/MemberRepository.java
+++ src/main/java/study/datapa/repository/MemberRepository.java
@@ -2,11 +2,14 @@ package study.datapa.repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import study.datapa.entity.Member;
+
+import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
@@ -22,4 +25,23 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
                                             // 또는 clearAutomatically를 적용해야한다
     @Query("update Member m set m.age = m.age +1 where m.age >= :age")
     int bulkAgePlus(@Param("age") int age);
+
+    @Query("select m" +
+            " from Member m" +
+            " join fetch m.team")
+    List<Member> findMemberFetchJoin();
+
+    @Override
+    @EntityGraph(attributePaths = {"team"}) // fetch join을 쿼리 없이 실
+    List<Member> findAll();
+
+    @EntityGraph(attributePaths = {"team"})
+    @Query("select m" +
+            " from Member m")
+    List<Member> findMemberEntityGraph();
+
+    @EntityGraph(attributePaths = {"team"})
+    List<Member> findByUsername(@Param("username") String username);
+
+
 }
--- src/test/java/study/datapa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datapa/repository/MemberRepositoryTest.java
@@ -8,6 +8,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datapa.entity.Member;
+import study.datapa.entity.Team;
+
+import javax.persistence.EntityManager;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,6 +24,12 @@ class MemberRepositoryTest {
 
     @Autowired
     MemberRepository memberRepository;
+
+    @Autowired
+    TeamRepository teamRepository;
+
+    @Autowired
+    EntityManager em;
 
     @Test
     public void testMember() throws Exception {
@@ -66,4 +77,78 @@ class MemberRepositoryTest {
             System.out.println("member : " + member1);
         }
     }
+
+    @Test
+    public void findMemberLazy() {
+        //given
+        Team teamA = Team.builder()
+                .name("teamA")
+                .build();
+        Team teamB = Team.builder()
+                .name("teamB")
+                .build();
+
+        teamRepository.save(teamA);
+        teamRepository.save(teamB);
+        memberRepository.save(Member.builder()
+                .username("member1")
+                .age(10)
+                .team(teamA)
+                .build());
+        memberRepository.save(Member.builder()
+                .username("member2")
+                .age(10)
+                .team(teamB)
+                .build());
+
+        em.flush();
+        em.clear();
+
+        //when
+
+        List<Member> all = memberRepository.findAll();
+        for (Member member : all) {
+            System.out.println("member = " + member.getUsername());
+            // N + 1
+            System.out.println("member.team = " + member.getTeam().getName());
+        }
+    }
+
+    @Test
+    public void findMemberfetchJoin() {
+        //given
+        Team teamA = Team.builder()
+                .name("teamA")
+                .build();
+        Team teamB = Team.builder()
+                .name("teamB")
+                .build();
+
+        teamRepository.save(teamA);
+        teamRepository.save(teamB);
+        memberRepository.save(Member.builder()
+                .username("member1")
+                .age(10)
+                .team(teamA)
+                .build());
+        memberRepository.save(Member.builder()
+                .username("member2")
+                .age(10)
+                .team(teamB)
+                .build());
+
+        em.flush();
+        em.clear();
+
+        //when
+
+        List<Member> all = memberRepository.findMemberFetchJoin();
+        for (Member member : all) {
+            System.out.println("member = " + member.getUsername());
+            // N + 1
+            System.out.println("member.team = " + member.getTeam().getName());
+        }
+    }
+
+
 }
